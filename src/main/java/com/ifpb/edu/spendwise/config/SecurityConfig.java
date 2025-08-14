c2a2d5154700e68ef41e9feac6599d39a8afd7fb
lukilme <kilmerluisluk12@gmail.com>
2025-08-13 21:13:24 -0300

    feat: add authorization and authentication

--- src/main/java/com/ifpb/edu/spendwise/config/SecurityConfig.java
+++ src/main/java/com/ifpb/edu/spendwise/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +14,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.ifpb.edu.spendwise.model.Customer;
@@ -28,7 +26,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomerRepository customerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
